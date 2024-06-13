2faa35efa52a73a35a5df6dcaeef16d5f3d3622b
VarshiniD29 <varshinidunnala@gmail.com>
2024-06-13 12:54:02 -0400

    binding events, attributes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,33 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishlist';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   items : WishItem[] = [
-    new WishItem('Learn Angular'),
-    new WishItem('Finish Project', true),
-    new WishItem('Drink Water')
+   // new WishItem('Learn Angular'),
+   // new WishItem('Finish Project', true),
+   // new WishItem('Drink Water')
   ]
+
+  newWishText = '';
   title = 'wishlist';
+
+  addNewWish(){
+    //adding wishes to array
+    //clear the textbox
+    this.items.push(new WishItem(this.newWishText))
+    this.newWishText='';
+  }
+  toggleItem(item : WishItem){
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  }
 }
