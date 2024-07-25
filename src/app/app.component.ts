ef7bb6d627ec041730cb7d1a16015f5be15eb17e
VarshiniD29 <varshinidunnala@gmail.com>
2024-07-25 14:48:13 -0400

    ngmodelchange

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,19 +13,30 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppComponent {
   items : WishItem[] = [
-   // new WishItem('Learn Angular'),
-   // new WishItem('Finish Project', true),
-   // new WishItem('Drink Water')
+    new WishItem('Learn Angular'),
+    new WishItem('Finish Project', true),
+    new WishItem('Drink Water')
   ]
 
+  listFilter : String = '0';
   newWishText = '';
   title = 'wishlist';
+  visibleItems : WishItem[] = this.items;
 
   addNewWish(){
     //adding wishes to array
     //clear the textbox
     this.items.push(new WishItem(this.newWishText))
     this.newWishText='';
+  }
+  filterChanged(value: any){
+    if (value === '0') {
+      this.visibleItems = this.items;
+    } else if (value === '1') {
+      this.visibleItems = this.items.filter(item => !item.isComplete);
+    } else {
+      this.visibleItems = this.items.filter(item => item.isComplete);
+    }
   }
   toggleItem(item : WishItem){
     item.isComplete = !item.isComplete;
